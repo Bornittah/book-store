387ab40fc51960845f17d4fc5b94a8f7cad1718f
bornittah <bornittah@gmail.com>
2022-07-23 20:36:38 +0300

    Remove console statements

--- src/api/bookAPI.jsx
+++ src/api/bookAPI.jsx
@@ -1,10 +1,4 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux/es/exports';
-// eslint-disable-next-line import/no-cycle
-// import { fetchBooks } from '../redux/books/books';
-
 const API_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
-// const dispatch = useDispatch();
 
 export const appId = async () => {
   const response = await fetch(`${API_URL}/apps/`,
@@ -24,18 +18,16 @@ export const appId = async () => {
   return id;
 };
 
-// App ID
 export const fetchAppId = () => {
   const key = localStorage.getItem('book-store');
   if (key === null) {
     appId();
     const newKey = localStorage.getItem('book-store');
-    console.log(newKey);
     return newKey;
   }
   return key;
 };
-// app id from the local storage
+
 const id = fetchAppId();
 export const getBooks = async () => {
   const response = await fetch(`${API_URL}/apps/${id}/books`,
@@ -44,7 +36,7 @@ export const getBooks = async () => {
     });
   const bookList = [];
   const books = await response.json().then((data) => data)
-    .catch((err) => { console.log('Error', err); });
+    .catch((err) => err);
   Object.keys(books).forEach((book) => {
     books[book][0].id = book;
     bookList.push(books[book][0]);
@@ -59,7 +51,7 @@ export const postBook = async (bookdetails) => {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(bookdetails),
     });
-  const book = await response.json().then((data) => data).catch((err) => { console.log('Error', err); });
+  const book = await response.json().then((data) => data).catch((err) => err);
   return book;
 };
 
